d9aa52f7cef757a6111e394ec32aaf12068df902
Pedro Gomes do Nascimento <pedro030401@gmail.com>
2024-09-13 23:12:53 -0500

    Initial setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,18 +8,18 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAPpn1jTSj0FQ_HQ3j3ScTWNUBWtGWvFuA",
-  authDomain: "fairtechweb-dd440.firebaseapp.com",
-  projectId: "fairtechweb-dd440",
-  storageBucket: "fairtechweb-dd440.appspot.com",
-  messagingSenderId: "4896251289",
-  appId: "1:4896251289:web:580a4add8a225d69578708",
-  measurementId: "G-RD316T32F5"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
